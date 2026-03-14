c3b969195eccab5911dfa814d42e56043bed5064
Prashanth <prashanthraju506@gmail.com>
2026-03-14 06:18:19 +0000

    Changes for code quality

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,3 @@
-from services.cloud_node import CloudNode
 import pytest
 
 
@@ -28,16 +27,19 @@ def test_cache_file(edge_node):
     assert result["status"] == "cached"
     assert result["synced"] == False
 
+
 def test_cache_count(edge_node):
-    result = edge_node.cache_file("sample.py", "This is a sample python file")
+    edge_node.cache_file("sample.py", "This is a sample python file")
     value = edge_node.get_pending_sync_count()
     assert value == 1
 
+
 def test_sync_to_cloud(cloud_node, edge_node):
-    main = edge_node.cache_file("Difficult_file.apk", "Should see whether it works or not")
+    edge_node.cache_file("Difficult_file.apk", "Should see whether it works or not")
     result = edge_node.sync_to_cloud(cloud_node)
     assert "Difficult_file.apk" in result["synced"]
     assert edge_node.get_pending_sync_count() == 0
+
 
 @pytest.mark.parametrize("filename, content", [
     ("report.txt", "This is the first file"),
@@ -55,30 +57,36 @@ def test_storage_manager(storage_manager):
     assert result["sync_status"] == "success"
     assert result["cache_status"] == "cached"
 
+
 def test_read_storage(storage_manager):
-    result = storage_manager.write_file("test.txt", "Hello file")
+    storage_manager.write_file("test.txt", "Hello file")
     read_result = storage_manager.read_file(filename="test.txt")
     assert read_result["source"] == "edge_cache"
+
 
 def test_node_health(storage_manager):
     result = storage_manager.get_system_health()
     assert result["cloud_node"]["status"] == "online"
     assert result["total_operations"] == 0
 
+
 def test_storage_nohint(storage_manager):
     result = storage_manager.write_file("first_test.csv", "Here is my own test case")
     assert result["filename"] == "first_test.csv"
+
 
 def test_storage_nohint2(storage_manager):
     storage_manager.write_file("second.txt", "Pytest")
     read_result = storage_manager.read_file("second.txt")
     assert read_result["data"]["content"] == "Pytest"
 
+
 def test_edgenode_nohint(edge_node):
     edge_node.cache_file("hello.java", "What is this?")
     edge_node.simulate_failure()
     with pytest.raises(ConnectionError):
         edge_node.cache_file("hello.py", "This is a python file")
+
 
 @pytest.mark.parametrize("filename, content", [
     ("text.py", "Hello Mars"),
@@ -89,6 +97,7 @@ def test_cloudnode_nohint(cloud_node, filename, content):
     cloud_node.upload_file(filename, content)
     result = cloud_node.list_files()
     assert len(result) == 1
+
 
 @pytest.mark.parametrize("filename, content", [
     ("test.py", "I am here"),
